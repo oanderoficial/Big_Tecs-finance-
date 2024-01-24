e583e217d2c556ab4f9c827cf2f8fadf51bd5ca2
Anderson B Silva (Ander) <32654298+oanderoficial@users.noreply.github.com>
2024-01-24 17:27:37 -0300

    update de comentários e remoção de arquivos.

--- main_plotly.py
+++ main_plotly.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import plotly.express as px
 
+#Grafico de linhas 
 def plotar_dados_acoes(df, nome_acao):
     fig = px.line(df, x='Date', y='Volume', title=f'Desempenho da Ação {nome_acao}', template='plotly_dark')
     fig.show()
 
+#Importando os dados em CSV 
 opcao = ''
 df_netflix = pd.read_csv('https://query1.finance.yahoo.com/v7/finance/download/NFLX?period1=1022112000&period2=1705881600&interval=1d&events=history&includeAdjustedClose=true')
 df_meta = pd.read_csv('https://query1.finance.yahoo.com/v7/finance/download/META?period1=1337299200&period2=1705881600&interval=1d&events=history&includeAdjustedClose=true')
 df_google = pd.read_csv('https://query1.finance.yahoo.com/v7/finance/download/GOOG?period1=1092873600&period2=1705881600&interval=1d&events=history&includeAdjustedClose=true')
 
+
+#Menu de opções
 while opcao != "0":
     print("1-Netflix, Inc. (NFLX)")
     print("2-Meta Platforms, Inc. (META)")
